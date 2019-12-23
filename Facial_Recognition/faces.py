058098eebb2cec121f5bab77ecb1edbae5487d16
Hassan Raza Bukhari <51364060+eruditehassan@users.noreply.github.com>
2019-12-23 13:49:04 +0500

    Update faces.py

--- Facial_Recognition/faces.py
+++ Facial_Recognition/faces.py
@@ -1,38 +1,44 @@
 import cv2
 import pickle
 
-face_cascade = cv2.CascadeClassifier('Cascades/data/haarcascade_frontalface_alt2.xml')
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read("trainer.yml")
-labels = {}
-with open("labels.pickle","rb") as f:
-    labels_in = pickle.load(f)
-labels = {y:x for x,y in labels_in.items()}
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-    for a,b,c,d in faces:
-        roi_gray = gray[b:b+d,a:a+c]
-        roi_color = frame[b:b+d,a:a+c]
-        #print(a,b,c,d)
-        id_,conf = recognizer.predict(roi_gray)
-        if conf>=45 and conf<=75:
-            print(id_)
-            print(labels[id_])
-            font = cv2.FONT_HERSHEY_COMPLEX
-            name = labels[id_]
-            color = (255,255,255)
+def facial_recognition():
+    return_name = ""
+    face_cascade = cv2.CascadeClassifier('Cascades/data/haarcascade_frontalface_alt2.xml')
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read("trainer.yml")
+    labels = {}
+    with open("labels.pickle","rb") as f:
+        labels_in = pickle.load(f)
+    labels = {y:x for x,y in labels_in.items()}
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+        for a,b,c,d in faces:
+            roi_gray = gray[b:b+d,a:a+c]
+            roi_color = frame[b:b+d,a:a+c]
+            #print(a,b,c,d)
+            id_,conf = recognizer.predict(roi_gray)
+            if conf>=45 and conf<=75:
+                print(id_)
+                print(labels[id_])
+                font = cv2.FONT_HERSHEY_COMPLEX
+                name = labels[id_]
+                return_name = name
+                color = (255,255,255)
+                stroke = 2
+                cv2.putText(frame, name, (a,b),font,1,color,stroke,cv2.LINE_AA)
+
+            img = "currentImage.png"
+            cv2.imwrite(img,roi_color)
+            color = (255,0,0)
             stroke = 2
-            cv2.putText(frame, name, (a,b),font,1,color,stroke,cv2.LINE_AA)
-        img = "Hassan.png"
-        cv2.imwrite(img,roi_color)
-        color = (255,0,0)
-        stroke = 2
-        cv2.rectangle(frame,(a,b),(a+c,b+d),color,stroke)
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-cap.release()
-cap.destroyAllWindows()
+            cv2.rectangle(frame,(a,b),(a+c,b+d),color,stroke)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            return return_name
+            break
+    #cap.release()
+    #cap.destroyAllWindows()
+facial_recognition()
